32fb2f9412ce902e3a72d99683780c24ffec1222
Muhammad Iqbal <webcracking@gmail.com>
2022-03-04 16:15:18 +0800

    add exception to csrf token

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/gettoken', 'App\Http\Controllers\MainCtrl@obtain_token');
+Route::post('/tts', 'App\Http\Controllers\MainCtrl@getTTS');
+Route::get('/testts', 'App\Http\Controllers\MainCtrl@getTTS');
+Route::get('/tts', 'App\Http\Controllers\MainCtrl@tesTTS');
+Route::post('/products', 'App\Http\Controllers\ProductCtrl@productByName');
+Route::post('/upload', 'App\Http\Controllers\ProductCtrl@imgUpload');
+Route::get('/productsbyimg', 'App\Http\Controllers\ProductCtrl@productByPicture');
+Route::get('/tesfungsi', 'App\Http\Controllers\ProductCtrl@tesfungsi');
+Route::get('/tesfungsi2', 'App\Http\Controllers\ImageCtrl@tesfungsi');
+Route::get('/teslagi', 'App\Http\Controllers\ImageCtrl@teslagi');
+Route::get('/covid', 'App\Http\Controllers\CovidCtrl@getDataCovid');
