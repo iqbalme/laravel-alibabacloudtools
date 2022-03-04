38f5d6e94e7302f0f55486a186c8c9d24c1c958d
Muhammad Iqbal <webcracking@gmail.com>
2022-03-04 15:53:36 +0800

    set imageupload from frontend

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,13 @@ Route::get('/', function () {
 });
 
 Route::get('/gettoken', 'App\Http\Controllers\MainCtrl@obtain_token');
-Route::post('/tts', 'MainCtrl@getTTS');
-Route::get('/testts', 'MainCtrl@getTTS');
-Route::get('/tts', 'MainCtrl@tesTTS');
-Route::post('/products', 'ProductCtrl@productByName');
-Route::post('/upload', 'ProductCtrl@imgUpload');
-Route::get('/productsbyimg', 'ProductCtrl@productByPicture');
+Route::post('/tts', 'App\Http\Controllers\MainCtrl@getTTS');
+Route::get('/testts', 'App\Http\Controllers\MainCtrl@getTTS');
+Route::get('/tts', 'App\Http\Controllers\MainCtrl@tesTTS');
+Route::post('/products', 'App\Http\Controllers\ProductCtrl@productByName');
+Route::post('/upload', 'App\Http\Controllers\ProductCtrl@imgUpload');
+Route::get('/productsbyimg', 'App\Http\Controllers\ProductCtrl@productByPicture');
 Route::get('/tesfungsi', 'App\Http\Controllers\ProductCtrl@tesfungsi');
 Route::get('/tesfungsi2', 'App\Http\Controllers\ImageCtrl@tesfungsi');
 Route::get('/teslagi', 'App\Http\Controllers\ImageCtrl@teslagi');
-Route::get('/covid', 'CovidCtrl@getDataCovid');
+Route::get('/covid', 'App\Http\Controllers\CovidCtrl@getDataCovid');
